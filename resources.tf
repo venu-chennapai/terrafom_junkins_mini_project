dca165acacea1f179241dfe5047cf9c0a085b330
venugopalvarma <87475479+venugopalvarma@users.noreply.github.com>
2024-11-17 23:31:54 +0530

    Commit_file_1.47

--- resources.tf
+++ resources.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "PROD" {
-    ami = "ami-063d43db0594b521b"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "PROD"
-    } 
+  ami           = "ami-012967cc5a8c9f891"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "PROD"
+  }
 }
